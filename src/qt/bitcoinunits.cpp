370cc71f9f8099de83c0ec37c60a333f9dc85145
sveing <svein2122@gmail.com>
2014-05-17 12:15:24 +0200

    changed occurrences of litecoin with utex etc

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("UTX");
+    case mBTC: return QString("mUTX");
+    case uBTC: return QString::fromUtf8("μUTX");
     default: return QString("???");
     }
 }
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Utexs");
+    case mBTC: return QString("Milli-Utexs (1 / 1,000)");
+    case uBTC: return QString("Micro-Utexs (1 / 1,000,000)");
     default: return QString("???");
     }
 }
